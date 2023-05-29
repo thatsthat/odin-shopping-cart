a93f26a6a4fbebf6487d0abae33bf6a8ea2107d3
Aram <aram.anto@gmail.com>
2023-05-29 15:24:41 +0200

    started implementing product quantities state

--- src/components/Eshop.js
+++ src/components/Eshop.js
@@ -1,10 +1,45 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Products from "./Products";
 import styles from "../styles/Eshop.module.css";
 
-function Eshop() {
+const Eshop = () => {
+  let initialCounters = [0, 0, 0, 0];
+  const [counters, setCounters] = useState(initialCounters);
+
+  function handleIncrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        // Increment the clicked counter
+        return c + 1;
+      } else {
+        // The rest haven't changed
+        return c;
+      }
+    });
+    setCounters(nextCounters);
+  }
+
+  function handleDecrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        // Decrement the clicked counter
+        if (c === 0) return c;
+        else return c - 1;
+      } else {
+        // The rest haven't changed
+        return c;
+      }
+    });
+    setCounters(nextCounters);
+  }
+
+  useEffect(() => {
+    (() => {})();
+  }, []);
+
   return (
     <div className={styles.main}>
       <BrowserRouter>
@@ -16,6 +51,6 @@ function Eshop() {
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default Eshop;
